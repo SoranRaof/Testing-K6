6d87ff7caab34f5e4a077eb91de206a287f66ecc
Soran Raof <110354376+SoranRaof@users.noreply.github.com>
2023-10-02 11:54:29 +0100

    further research

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ import { sleep } from "k6";
 // VUs are essentially parallel while(true) loops. Scripts are written in JavaScript, as ES6 modules
 // So you can break larger tests into smaller pieces or make reusable pieces as you like
 
-export default function () {
-  http.get("http://test.k6.io");
+// uncomment the below code and comment the code below:
+// export default function () {
+//   http.get("http://test.k6.io");
 
-  // sleep is a built-in function that takes a duration argument (in seconds)
-  // you can also run the command k6 run --vus 2 --duration 30s script.js to configure the number of VUs and duration
-  sleep(10);
-}
+//   // sleep is a built-in function that takes a duration argument (in seconds)
+//   // you can also run the command k6 run --vus 2 --duration 30s script.js to configure the number of VUs and duration
+//   sleep(10);
+// }
+// uncomment the code above ^^
+
+// configuring the number of VUs and duration in the script
+//uncomment the below code and comment the below code:
+
+// export const options = {
+//   vus: 10,
+//   duration: "30s",
+// };
+
+// export default function () {
+//   http.get("http://test.k6.io");
+//   sleep(10);
+// }
+// uncomment the code above ^^
